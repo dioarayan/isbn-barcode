5284bec041329e32ceda2f13b8a8d7ebee5ef459
Dio Arayan <drarayan1994@gmail.com>
2023-08-18 15:28:38 +0800

    finalize turbo feature plus add styling

--- app/controllers/isbn_controller.rb
+++ app/controllers/isbn_controller.rb
@@ -1,18 +1,29 @@
 class IsbnController < ApplicationController
+  before_action :check_for_cancel
 
-  def index     
+  def index    
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end 
 
   def calculate_isbn
     @final_result = Calculate::CalculateDigits.call(isbn_input: get_input)
     respond_to do |format|
-        format.turbo_stream{ render turbo_stream: turbo_stream.update("isbn_result", 
-          partial: 'isbn/final_result', locals: {final_result: @final_result})}
-        format.html { render :index, alert: "Successfully calculated ISBN!" }
+      format.turbo_stream
+      format.html { render :index, alert: "Successfully calculated ISBN!" }
     end
       rescue CalculateDigitsException => e
       redirect_to root_path, alert: e.formatting_error_message
   end
+
+  def check_for_cancel
+    if params[:commit] == "Cancel"
+      redirect_to root_path
+    end
+  end
+  
 
   private
 
